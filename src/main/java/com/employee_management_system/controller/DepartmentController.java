012eb99522332617240065fc83ba227b66d8b79c
Raju Gharti <gharti.rg@gmail.com>
2023-11-21 12:20:31 -0500

    implement crud operation for department rest apis

--- src/main/java/com/employee_management_system/controller/DepartmentController.java
+++ src/main/java/com/employee_management_system/controller/DepartmentController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/departments")
 @AllArgsConstructor
@@ -20,5 +22,33 @@ public class DepartmentController {
         DepartmentDto department = departmentService.createDepartment(departmentDto);
 
         return new ResponseEntity<>(department, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DepartmentDto> getDepartmentById(@PathVariable Long id) {
+        DepartmentDto department = departmentService.getDepartmentById(id);
+
+        return ResponseEntity.ok(department);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentDto>> getAllDepartments() {
+        List<DepartmentDto> departments = departmentService.getAllDepartments();
+
+        return ResponseEntity.ok(departments);
+    }
+
+    @PutMapping("/{id}/update")
+    public ResponseEntity<DepartmentDto> updateDepartment(@PathVariable Long id, @RequestBody DepartmentDto departmentDto) {
+        DepartmentDto department = departmentService.updateDepartment(id, departmentDto);
+
+        return ResponseEntity.ok(department);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<String> deleteDepartment(@PathVariable Long id) {
+        departmentService.deleteDepartment(id);
+
+        return ResponseEntity.ok("Department deleted successfully");
     }
 }
--- src/main/java/com/employee_management_system/service/implementation/DepartmentServiceImplementation.java
+++ src/main/java/com/employee_management_system/service/implementation/DepartmentServiceImplementation.java
@@ -2,11 +2,15 @@ package com.employee_management_system.service.implementation;
 
 import com.employee_management_system.dto.DepartmentDto;
 import com.employee_management_system.entity.Department;
+import com.employee_management_system.exception.ResourceNotFoundException;
 import com.employee_management_system.mapper.DepartmentMapper;
 import com.employee_management_system.repository.DepartmentRepository;
 import com.employee_management_system.service.DepartmentService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -21,5 +25,41 @@ public class DepartmentServiceImplementation implements DepartmentService {
         Department savedDepartment = departmentRepository.save(department);
 
         return DepartmentMapper.mapToDepartmentDto(savedDepartment);
+    }
+
+    @Override
+    public DepartmentDto getDepartmentById(Long id) {
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Department doesn't exist with the given id: " + id));
+
+        return DepartmentMapper.mapToDepartmentDto(department);
+    }
+
+    @Override
+    public List<DepartmentDto> getAllDepartments() {
+        List<Department> departments = departmentRepository.findAll();
+        return departments.stream().map(DepartmentMapper::mapToDepartmentDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public DepartmentDto updateDepartment(Long id, DepartmentDto departmentDto) {
+
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Department doesn't exist with the given id: " + id));
+
+        department.setName(departmentDto.getName());
+        department.setDescription(departmentDto.getDescription());
+
+        Department updateDepartment = departmentRepository.save(department);
+
+        return DepartmentMapper.mapToDepartmentDto(updateDepartment);
+    }
+
+    @Override
+    public void deleteDepartment(Long id) {
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Department doesn't exist with the given id: " + id));
+
+        departmentRepository.delete(department);
     }
 }
